5f9be873dbab8032fe081ce7d76479c0ef77511d
flamebarke <39644720+flamebarke@users.noreply.github.com>
2021-03-10 00:28:52 +1100

    Update serpico_installer.sh

--- serpico_installer.sh
+++ serpico_installer.sh
@@ -25,7 +25,9 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 
 echo "[*] Installing rvm"
 sleep 3
-curl -sSL https://get.rvm.io | bash -s stable 
+curl -sSL https://get.rvm.io | bash -s stable
+
+source ~/.rvm/scripts/rvm
 
 #echo "[*] Adding user to rvm group"
 #sudo usermod -a -G rvm `whoami` 
